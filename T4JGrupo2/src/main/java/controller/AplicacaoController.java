defe6570e7bbe4da38d6b89059321f157d690d7d
Bárbara Leston Bandeira <blestonbandeira@upskill.pt>
2021-01-29 12:32:24 +0000

    nomes dos packages

--- T4JGrupo2/src/main/java/controller/AplicacaoController.java
+++ T4JGrupo2/src/main/java/controller/AplicacaoController.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Repositorios;
+package controller;
 
 /**
  *
  * @author acris
  */
-public class RepositorioOrganizacao {
+public class AplicacaoController {
     
 }
--- T4JGrupo2/src/main/java/repositorios/RepositorioUtilizador.java
+++ T4JGrupo2/src/main/java/repositorios/RepositorioUtilizador.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Repositorios;
+package repositorios;
 
 /**
  *
  * @author acris
  */
-public class RepositorioEmail {
+public class RepositorioUtilizador {
     
 }
