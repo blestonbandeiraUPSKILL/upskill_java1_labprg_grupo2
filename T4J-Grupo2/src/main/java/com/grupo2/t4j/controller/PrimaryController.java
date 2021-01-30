f880a62c45d3d539a05fd3a6dbb5710a12568588
Bárbara Leston Bandeira <blestonbandeira@upskill.pt>
2021-01-30 16:18:12 +0000

    projecto com javafx

---
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/controller/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.grupo2.t4j.grupo2;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
