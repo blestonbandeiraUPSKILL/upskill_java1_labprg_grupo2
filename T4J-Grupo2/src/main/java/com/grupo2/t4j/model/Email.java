69237bd37c850a31ca63de8eed25f77fd1b0a092
Caroline Duarte <74610624+carol-duarte@users.noreply.github.com>
2021-02-01 15:30:31 +0000

    Classe Organização

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Email.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Email.java
@@ -7,8 +7,44 @@ package com.grupo2.t4j.model;
 
 /**
  *
- * @author acris
+ * @author CAD
  */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Email {
+   
+    private String email;
     
+    public Email (String email){
+        setEmail(email);
+    }
+    
+    public Email (Email email){
+        setEmail(email.email);
+    }
+    public void setEmail(String email){
+        if(eEmailValido(email)){
+            this.email = email;
+        }
+        else{
+            throw new IllegalArgumentException("O email é inválido!");
+        }
+    }
+    
+    // https://receitasdecodigo.com.br/java/validar-email-em-java
+    
+    public static boolean eEmailValido(String email) {
+        boolean eValido = false;
+        if (email != null && email.length() > 0) {
+            String expressao = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+            Pattern pattern = Pattern.compile(expressao, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(email);
+            if (matcher.matches()) {
+                eValido = true;
+            }
+        }
+        return eValido;
+    }
 }
