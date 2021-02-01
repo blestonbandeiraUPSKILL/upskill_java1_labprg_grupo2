8b26e774b69120a7dddd43e719f96dad5cb23ccd
Caroline Duarte <74610624+carol-duarte@users.noreply.github.com>
2021-02-01 18:08:09 +0000

    Classe Organização

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Password.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Password.java
@@ -7,8 +7,37 @@ package com.grupo2.t4j.model;
 
 /**
  *
- * @author acris
+ * @author CAD
  */
 public class Password {
+
+    private String password;
+    
+    public Password (String password){
+        setPassword(password);
+    }
+    
+    public Password (Password password){
+        setPassword(password.password);
+    }
+    
+    public void setPassword(String password){
+        if(eSenhaValida(password)){
+            this.password = password;
+        }
+        else{
+            throw new IllegalArgumentException("A senha informada não é válida!");
+        }
+    }
+        
+    private static boolean eSenhaValida(String password){
+        if(password == null){
+            return false;
+        }
+        if(password.length()<8){
+            return false;
+        }
+        return true;
+    }
     
 }
