433fbc6e8342edd8159cd45c856722f5d55a9bb7
Ana Batista Pereira <anacbpereira91@upskill.pt>
2021-02-01 11:06:14 +0000

    UC3 implementação

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Colaborador.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Colaborador.java
@@ -10,5 +10,11 @@ package com.grupo2.t4j.model;
  * @author acris
  */
 public class Colaborador {
+
+    Colaborador(String diogo_Ventura, String diogorochacom, String paSsWoRd, String rh, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
     
 }
--- T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Organizacao.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Organizacao.java
@@ -10,5 +10,20 @@ package com.grupo2.t4j.model;
  * @author acris
  */
 public class Organizacao {
+
+    Organizacao(String casa_Rocha, String string, String casarochapt, String string0, String casarochacom) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addColaborador(Colaborador c1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    Object getColaboradores() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
     
 }
