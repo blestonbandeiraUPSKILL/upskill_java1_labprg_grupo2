f17fa7c5bb9933f289f99d5a7ee861d0f42509f0
Caroline Duarte <74610624+carol-duarte@users.noreply.github.com>
2021-02-02 12:23:32 +0000

    Testes - tentativa

---
+++ T4J-Grupo2/src/test/java/com/grupo2/t4j/repository/RepositorioUtilizadorTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupo2.t4j.repository;
+
+/**
+ *
+ * @author CAD
+ */
+
+import com.grupo2.t4j.model.*;
+import com.grupo2.t4j.repository.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RepositorioUtilizadorTest {
+
+    public RepositorioUtilizadorTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void testAddUtilizador() {
+        //Arrange
+        RepositorioUtilizador ru1 = new RepositorioUtilizador();
+        Utilizador u1= new Utilizador("Carol", "carol@upskill.pt", "12345678");
+        //Act
+        u1.addUtilizador(ru1);
+        
+        //Assert
+        assertTrue(ru1.getUtilizadores().contains(u1));
+    }
+
+    @Test
+    public void testSetListaUtilizadores() {
+    }
+
+    @Test
+    public void testGetUtilizadores() {
+    }
+
+    @Test
+    public void testGetUtilizadorByEmail() {
+    }
+}
