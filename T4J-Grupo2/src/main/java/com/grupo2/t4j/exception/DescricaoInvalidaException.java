5b741ab9ac043c277b8890c65cb503d1828fa8ba
Ana Batista Pereira <anacbpereira91@upskill.pt>
2021-02-02 11:11:54 +0000

    update competencia tecnica

---
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/exception/DescricaoInvalidaException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupo2.t4j.exception;
+
+/**
+ *
+ * @author acris
+ */
+public class DescricaoInvalidaException extends RuntimeException{
+    public DescricaoInvalidaException(String s) {
+        super(s);
+    }
+}
