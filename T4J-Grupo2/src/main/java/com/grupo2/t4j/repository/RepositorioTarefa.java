a0938cb001211c267e6320ce339cb4e142f32792
Ana Batista Pereira <anacbpereira91@upskill.pt>
2021-02-02 17:27:12 +0000

    UPDATE RepositorioTarefa

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/repository/RepositorioTarefa.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/repository/RepositorioTarefa.java
@@ -1,4 +1,14 @@
 package com.grupo2.t4j.repository;
 
 public class RepositorioTarefa {
+    private static RepositorioTarefa instance;
+    
+    
+    public static RepositorioTarefa getInstance(){
+        if (instance == null){
+            instance = new RepositorioTarefa();
+        }
+        return instance;
+    }
+    
 }
