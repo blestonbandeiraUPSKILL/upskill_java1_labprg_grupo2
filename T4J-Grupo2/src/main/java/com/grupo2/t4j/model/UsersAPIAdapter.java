ee54e70ea8bfa2f665656677c9795a2131f4ea6f
Bárbara Leston Bandeira <blestonbandeira@upskill.pt>
2021-02-02 14:03:38 +0000

    update

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/model/UsersAPIAdapter.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/model/UsersAPIAdapter.java
@@ -1,6 +1,7 @@
 package com.grupo2.t4j.model;
 
 import com.grupo2.t4j.network.*;
+import org.json.JSONObject;
 
 public class UsersAPIAdapter {
 
@@ -20,8 +21,41 @@ public class UsersAPIAdapter {
             switch (httpResponse.getStatus()) {
 
                 case HttpStatusCode.OK:
-                    brea
+                    break;
+
+                case HttpStatusCode.Conflict:
+                    break;
             }
+            JSONObject bodyJSON = new JSONObject(httpResponse.getBody().replaceAll("\\[|\\]", ""));
+            app_context = bodyJSON.getString("app_context");
+        }
+        return app_context;
+    }
+
+    public boolean login(String user_id, Password password) {
+        String url = "/login?app_contect=" + getContext() + "?username=" + user_id + "?password=" + password;
+        HttpRequest httpRequest = new HttpRequest(HttpRequestType.POST, url, "");
+        HttpResponse httpResponse = HttpConnection.makeRequest(httpRequest);
+
+        switch (httpResponse.getStatus()) {
+            case HttpStatusCode.OK:
+                return true;
+            case HttpStatusCode.Conflict:
+                return false
+        }
+        return false;
+    }
+
+    public boolean logout() {
+        String url = "/logout?app_context=" + getContext();
+        HttpRequest httpRequest = new HttpRequest(HttpRequestType.POST, url, "");
+        HttpResponse httpResponse = HttpConnection.makeRequest(httpRequest);
+
+        switch (httpResponse.getStatus()) {
+            case HttpStatusCode.OK:
+                return true;
+
+
         }
     }
 }
