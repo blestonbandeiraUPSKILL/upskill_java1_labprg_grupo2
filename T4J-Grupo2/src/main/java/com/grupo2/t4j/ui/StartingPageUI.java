ff606380ca21a81392b982ed52ece56d22d26f2d
Bárbara Leston Bandeira <blestonbandeira@upskill.pt>
2021-02-03 14:49:37 +0000

    login

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/StartingPageUI.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/StartingPageUI.java
@@ -25,6 +25,9 @@ public class StartingPageUI implements Initializable {
     private Stage adicionarStage;
     private Scene sceneRegisterOrg;
     private Scene sceneLogin;
+    private Scene sceneGestor;
+    private Scene sceneAdministrativo;
+    private Scene sceneColaborador;
     @FXML
     TextField txtEmailLogin;
     @FXML
@@ -39,14 +42,23 @@ public class StartingPageUI implements Initializable {
         try {
             FXMLLoader loaderLogin = new FXMLLoader(getClass().getResource("/com/grupo2/t4j/fxml/JanelaPrincipalScene.fxml"));
             Parent rootLogin = loaderLogin.load();
+            sceneLogin = new Scene(rootLogin);
 
             FXMLLoader loaderRegister = new FXMLLoader(getClass().getResource("/com/grupo2/t4j/fxml/RegisterOrgScene.fxml"));
             Parent rootRegisterOrg = loaderRegister.load();
+            sceneRegisterOrg = new Scene(rootRegisterOrg);
 
             FXMLLoader loaderGestor = new FXMLLoader(getClass().getResource("/com/grupo2/t4j/fxml/GestorLogadoScene.fxml"));
+            Parent rootGestor = loaderGestor.load();
+            sceneGestor = new Scene(rootGestor);
 
-            sceneLogin = new Scene(rootLogin);
-            sceneRegisterOrg = new Scene(rootRegisterOrg);
+            FXMLLoader loaderColaborador = new FXMLLoader(getClass().getResource("/com/grupo2/t4j/fxml/ColaboradorLogadoScene.fxml"));
+            Parent rootColaborador = loaderColaborador.load();
+            sceneColaborador = new Scene(rootColaborador);
+
+            FXMLLoader loaderAdministrativo = new FXMLLoader(getClass().getResource("/com/grupo2/t4j/fxml/AdministrativoLogadoScene.fxml"));
+            Parent rootAdministrativo = loaderAdministrativo.load();
+            sceneAdministrativo = new Scene(rootAdministrativo);
 
             adicionarStage = new Stage();
             adicionarStage.initModality(Modality.APPLICATION_MODAL);;
@@ -72,13 +84,32 @@ public class StartingPageUI implements Initializable {
         adicionarStage.show();
     }
 
-    public void login(ActionEvent actionEvent) {
-        usersAPIAdapter.login(txtEmailLogin.getText(), new Password(txtPasswordLogin.getText()));
-        switch (applicationController.getRole()) {
-            case "gestor":
-                adicionarStage.setScene(sceneGestor);
-                adicionarStage.setTitle("T4J - Gestor da Organização");
-                adicionarStage.show();
+    public void login(ActionEvent actionEvent) throws IOException{
+
+        if(usersAPIAdapter.login(txtEmailLogin.getText(), new Password(txtPasswordLogin.getText()))) {
+            switch (applicationController.getRole()) {
+                case "gestor":
+                    adicionarStage.setScene(sceneGestor);
+                    adicionarStage.setTitle("T4J - Gestor da Organização");
+                    adicionarStage.show();
+                    break;
+                case "administrativo":
+                    adicionarStage.setScene(sceneAdministrativo);
+                    adicionarStage.setTitle("T4J - Administrativo");
+                    adicionarStage.show();
+                    break;
+                case "colaborador":
+                    adicionarStage.setScene(sceneColaborador);
+                    adicionarStage.setTitle("T4J - Colaborador");
+                    adicionarStage.show();
+                    break;
+            }
+        }
+        else {
+            AlertsUI.criarAlerta(Alert.AlertType.ERROR,
+                    MainApp.TITULO_APLICACAO,
+                    "Erro ao validar os dados",
+                    "Dados de login inválidos");
         }
     }
 }
