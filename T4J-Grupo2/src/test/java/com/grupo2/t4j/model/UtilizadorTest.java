eee24309d80cdb7fd2e78bbb27040f5313dff0b1
Caroline Duarte <74610624+carol-duarte@users.noreply.github.com>
2021-02-04 16:57:56 +0000

    mais testes

--- T4J-Grupo2/src/test/java/com/grupo2/t4j/model/UtilizadorTest.java
+++ T4J-Grupo2/src/test/java/com/grupo2/t4j/model/UtilizadorTest.java
@@ -52,7 +52,7 @@ public class UtilizadorTest {
     @Test (expected = NomeInvalidoException.class)
     public void testCriarUtilizadorNomeIncorreto() {
         
-        Utilizador u2 = new Utilizador("Oi", "fulano@upskill.pt", "12345678");
+        Utilizador u2 = new Utilizador("", "fulano@upskill.pt", "12345678");
              
     }
     
@@ -65,7 +65,7 @@ public class UtilizadorTest {
     
     @Test (expected = IllegalArgumentException.class)
     public void testCriarUtilizadorPasswordFormaIncorreta() {
-        
+       
         Utilizador u4 = new Utilizador("Fulano", "fulano@upskill.pt", "123");
              
     }
