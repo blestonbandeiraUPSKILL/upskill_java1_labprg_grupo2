eadae3ba0644527e98a359a0864ad43f1ad423f9
Bárbara Leston Bandeira <blestonbandeira@upskill.pt>
2021-02-06 16:43:26 +0000

    área do colaborador

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/AdministrativoLogadoUI.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/AdministrativoLogadoUI.java
@@ -2,6 +2,9 @@ package com.grupo2.t4j.ui;
 
 import com.grupo2.t4j.controller.RegistarAreaActividadeController;
 import com.grupo2.t4j.model.AreaActividade;
+import com.grupo2.t4j.repository.RepositorioAreaActividade;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,6 +19,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class AdministrativoLogadoUI implements Initializable {
@@ -122,6 +127,7 @@ public class AdministrativoLogadoUI implements Initializable {
 
     public void updateListViewAreaActividade() {
         listaAreasAtividade.getItems().setAll(registarAreaActividadeController.getAreasActividade());
+        //listaAreasAtividade.setItems(FXCollections.observableArrayList(registarAreaActividadeController.getAreasActividade()));
 
     }
 
@@ -138,8 +144,6 @@ public class AdministrativoLogadoUI implements Initializable {
                     "Erro",
                     exception.getMessage());
         }
-
-        //listaAreasAtividade.setItems(FXCollections.observableArrayList(registarAreaActividadeController.getAreasActividade()));
 
         adicionarStage.setScene(sceneStartingPage);
         adicionarStage.setTitle(MainApp.TITULO_APLICACAO);
