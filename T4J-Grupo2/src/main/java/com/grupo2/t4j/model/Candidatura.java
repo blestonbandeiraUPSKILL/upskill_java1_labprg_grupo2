3bdba22d877dbd3cdd76b7359b87041dc65f5c77
Caroline Duarte <duarte.carol@upskill.pt>
2021-02-17 15:56:30 +0000

    continuacao

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Candidatura.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Candidatura.java
@@ -22,42 +22,43 @@ import java.util.List;
 public class Candidatura implements Serializable {
     
     /**
-     * 
+     * O id da Candidatura
      */
     private String idCandidatura;
     
     /**
-     * 
+     * A data da Candidatura
      */
     private Data dataCandidatura;
     
     /**
-     * 
+     * O valor pretendido pelo Freelancer na sua candidatura a uma dada Tarefa
      */
     private double valorPretendido;
     
     /**
-     * 
+     * O número de dias que o Freelancer indica para a realização de uma dada Tarefa
+     * a que se candidatou
      */
     private int numeroDias;
     
     /**
-     * 
+     * O texto de apresentação do Freelancer para sua candidatura a uma dada Tarefa
      */
     private String txtApresentacao;
     
     /**
-     * 
+     * O texto de motivação do Freelancer para sua candidatura a uma dada Tarefa
      */
     private String txtMotivacao;
     
     /**
-     * 
+     *  O texto de apresentação do Freelancer para uma candidatura por omissão
      */
     private static final String APRESENTACAO_POR_OMISSAO = "Sem apresentação";
     
     /**
-     * 
+     *  O texto de motivação do Freelancer para uma candidatura por omissão
      */
     private static final String MOTIVACAO_POR_OMISSAO = "Sem motivação";
     
@@ -69,12 +70,13 @@ public class Candidatura implements Serializable {
             cal.get(Calendar.DAY_OF_MONTH));
     
     /**
-     *
-     * @param idCandidatura
-     * @param valorPretendido
-     * @param numeroDias
-     * @param txtApresentacao
-     * @param txtMotivacao
+     * O construtor completo da classe Candidatura
+     * @param idCandidatura - o id da Candidatura
+     * @param valorPretendido - o valor pretendido pelo Freelancer na sua candidatura a uma dada Tarefa
+     * @param numeroDias - número de dias que o Freelancer indica para a realização de uma dada Tarefa
+     * a que se candidatou
+     * @param txtApresentacao - o texto de apresentação do Freelancer para sua candidatura a uma dada Tarefa
+     * @param txtMotivacao - o texto de motivação do Freelancer para sua candidatura a uma dada Tarefa
      */
     public Candidatura(String idCandidatura, double valorPretendido, int numeroDias,
     String txtApresentacao,String txtMotivacao){
@@ -87,10 +89,12 @@ public class Candidatura implements Serializable {
     }
     
     /**
-     *
-     * @param idCandidatura
-     * @param valorPretendido
-     * @param numeroDias
+     * Construtor da classe Candidatura com texto de Apresentação e de Motivação 
+     * com valores por omissão
+     * @param idCandidatura - o id da Candidatura
+     * @param valorPretendido - o valor pretendido pelo Freelancer na sua candidatura a uma dada Tarefa
+     * @param numeroDias - número de dias que o Freelancer indica para a realização de uma dada Tarefa
+     * a que se candidatou
      */
     public Candidatura(String idCandidatura, double valorPretendido, int numeroDias){
         setIdCandidatura(idCandidatura);
@@ -102,8 +106,8 @@ public class Candidatura implements Serializable {
     }
     
     /**
-     *
-     * @param candidatura
+     * Construtor da classe Candidatura 
+     * @param candidatura é do tipo da classe Candidatura
      */
     public Candidatura(Candidatura candidatura){
         setIdCandidatura(candidatura.idCandidatura);
@@ -134,7 +138,7 @@ public class Candidatura implements Serializable {
       
     /**
      * Define o valor pretendido pelo Freelancer para realização da Tarefa à qual
-     * se candidatou
+     * se candidatou em euros
      * @param valorPretendido - o valor pretendido pelo serviço, em euros
      */
     public void setValor(double valorPretendido){
@@ -148,7 +152,8 @@ public class Candidatura implements Serializable {
     }
     
     /**
-     *
+     * Define o número de dias que o Freelancer indica para a realização de uma dada Tarefa
+     * a que se candidatou
      * @param numeroDias
      */
     public void setDias(int numeroDias){
@@ -162,7 +167,7 @@ public class Candidatura implements Serializable {
     }
     
     /**
-     *
+     * Define o texto de apresentação do Freelancer para sua candidatura a uma dada Tarefa
      * @param txtApresentacao
      */
     public void setApresentacao(String txtApresentacao){
@@ -175,7 +180,7 @@ public class Candidatura implements Serializable {
     }
     
     /**
-     *
+     * Define o texto de motivação do Freelancer para sua candidatura a uma dada Tarefa
      * @param txtMotivacao
      */
     public void setMotivacao(String txtMotivacao){
@@ -188,39 +193,41 @@ public class Candidatura implements Serializable {
     }
     
     /**
-     *
-     * @return
+     * Devolve o id da Candidatura
+     * @return idCandidatura
      */
     public String getIdCandidatura(){
         return idCandidatura;
     }
     
     /**
-     *
-     * @return
+     * Devolve a data de Candidatura no formato Data
+     * @return dataCandidatura
      */
     public Data getDataCandidatura(){
         return dataCandidatura;
     }
     
     /**
-     *
-     * @return
+     * Devolve o valor pretendido pelo Freelancer para realização da Tarefa à qual
+     * se candidatou em euros
+     * @return valorPretendido
      */
     public double getValorPretendido(){
         return valorPretendido;
     }
     
     /**
-     *
-     * @return
+     * Devolve o número de dias que o Freelancer indica para a realização de uma dada Tarefa
+     * a que se candidatou
+     * @return numeroDias
      */
     public int getNumeroDias(){
         return numeroDias;
     }
     
     /**
-     *
+     * Devolve o texto de apresentação do Freelancer para sua candidatura a uma dada Tarefa
      * @return
      */
     public String getApresentacao(){
@@ -228,7 +235,7 @@ public class Candidatura implements Serializable {
     }
     
     /**
-     *
+     * Devolve o texto de motivação do Freelancer para sua candidatura a uma dada Tarefa
      * @return
      */
     public String getMotivacao(){
@@ -237,7 +244,9 @@ public class Candidatura implements Serializable {
     
     /**
      * Representação textual da classe Candidatura
-     * @return o id da candidatura
+     * @return o id da candidatura, a data da candidatura em formato texto, o valor pretendido
+     * pelo Freelancer para a realização da Tarefa, o número de dias para a realização da Tarefa,
+     * o texto de apresentação da Candidatura e o texto de Motivação para a Candidatura
      */   
     @Override
     public String toString(){
@@ -246,6 +255,5 @@ public class Candidatura implements Serializable {
                 + "%nApresentação: %s %nMotivação: %s", idCandidatura, 
                 dataCandidatura.toAnoMesDiaString(), valorPretendido,
                 numeroDias, txtApresentacao, txtMotivacao);
-    }
-    
+    }    
 }
