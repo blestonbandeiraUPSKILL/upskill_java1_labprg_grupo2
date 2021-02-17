4ec077986a61bedf6e8b6fc8909d5d38ce96e0c2
Caroline Duarte <duarte.carol@upskill.pt>
2021-02-17 17:34:40 +0000

    freelancer

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Freelancer.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/model/Freelancer.java
@@ -15,6 +15,10 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.List;
 
+/**
+ *
+ * @author carol
+ */
 public class Freelancer extends Utilizador implements Serializable{
     
     /**
@@ -42,6 +46,16 @@ public class Freelancer extends Utilizador implements Serializable{
      */
     private static final Rolename ROLENAME_POR_DEFINICAO = Rolename.FREELANCER;
     
+    /**
+     * Construtor completo da classe Freelancer
+     * @param email - o email do Freelancer no formato da classe Email
+     * @param nome - o nome do Freelancer
+     * @param password - a password do Freelancer no formato da classe Password
+     * @param rolename - o papel do Freelancer na plataforma (por definição é Freelancer)
+     * @param NIF - o NIF do Freelancer
+     * @param enderecoPostalFreelancer - o endereço postal do Freelancer no formato
+     * da classe EnderecoPostal
+     */
     public Freelancer(Email email, String nome, Password password, Rolename rolename,
             String NIF, EnderecoPostal enderecoPostalFreelancer){
         super(email, nome, password, ROLENAME_POR_DEFINICAO);
@@ -50,6 +64,15 @@ public class Freelancer extends Utilizador implements Serializable{
         setEndereco(enderecoPostalFreelancer);        
     }
     
+    /**
+     * Construtor da classe Freelancer sem a password 
+     * @param emailFree - o email do Freelancer no formato de texto
+     * @param nome - o nome do Freelancer
+     * @param rolename - o papel do Freelancer na plataforma (por definição é Freelancer)
+     * @param NIF - o NIF do Freelancer
+     * @param enderecoPostalFreelancer - o endereço postal do Freelancer no formato
+     * da classe EnderecoPostal
+     */
     public Freelancer(String emailFree, String nome, Rolename rolename,
             String NIF, EnderecoPostal enderecoPostalFreelancer){
         super(emailFree, nome);
@@ -59,7 +82,18 @@ public class Freelancer extends Utilizador implements Serializable{
         setEndereco(enderecoPostalFreelancer);        
     }
     
-     public Freelancer(String emailFree, String nome, Rolename rolename,
+    /**
+     * Construtor da classe Freelancer sem a password 
+     * @param emailFree - o email do Freelancer no formato de texto
+     * @param nome - o nome do Freelancer
+     * @param rolename - o papel do Freelancer na plataforma (por definição é Freelancer)
+     * @param NIF - o NIF do Freelancer
+     * @param arruamento - o arruamento do Endereço Postal do Freelancer
+     * @param numeroPorta - o número da porta do Endereço Postal do Freelancer
+     * @param localidade - a localidade do do Endereço Postal do Freelancer
+     * @param codigoPostal - o código postal do Endereço Postal do Freelancer
+     */
+    public Freelancer(String emailFree, String nome, Rolename rolename,
             String NIF, String arruamento, String numeroPorta, String localidade, 
             String codigoPostal){
         super(emailFree, nome);
@@ -71,7 +105,12 @@ public class Freelancer extends Utilizador implements Serializable{
         setEndereco(enderecoPostalFree);        
     }
      
-     public Freelancer(Freelancer freelancer){
+    /**
+     * Construtor da classe Freelancer que recebe como parâmetro outra instância 
+     * da classe Freelancer
+     * @param freelancer
+     */
+    public Freelancer(Freelancer freelancer){
         super(freelancer.getEmail(), freelancer.getNome(), freelancer.getPassword(), 
                 freelancer.getRolename());
         setNIF(freelancer.NIF);
@@ -79,7 +118,11 @@ public class Freelancer extends Utilizador implements Serializable{
         setEndereco(freelancer.enderecoPostalFreelancer);        
     }
     
-    
+    /**
+     * Define o NIF do Freelancer
+     * @param NIF
+     * @throws NifInvalidoException
+     */
     public void setNIF(String NIF)throws NifInvalidoException {
         long numNif = Long.parseLong(NIF);
         if (numNif >= 100000000 && numNif <= 999999999) {
@@ -109,14 +152,26 @@ public class Freelancer extends Utilizador implements Serializable{
         this.enderecoPostalFreelancer = enderecoPostalFreelancer;
     }
     
+    /**
+     * Devolve o NIF do Freelancer
+     * @return NIF
+     */
     public String getNIF(){
         return NIF;
     }
     
+    /**
+     * Devolve o telefone do Freelancer
+     * @return telefone
+     */
     public String getTelefone(){
         return telefone;
     }
     
+    /**
+     * Devolve o endereço postal do Freelancer
+     * @return enderecoPostalFreelancer
+     */
     public EnderecoPostal getEnderecoFreelancer(){
         return enderecoPostalFreelancer;
     }
