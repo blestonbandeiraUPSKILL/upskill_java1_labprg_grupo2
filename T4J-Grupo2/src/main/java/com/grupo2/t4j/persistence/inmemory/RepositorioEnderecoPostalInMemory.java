e0a82546bac0cb48311c0ee34cbdae977fb35fa9
Marta Silvestre <marta.silvestre@upskill.pt>
2021-02-18 16:32:21 +0000

    Update Repositório Endereço Postal

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/persistence/inmemory/RepositorioEnderecoPostalInMemory.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/persistence/inmemory/RepositorioEnderecoPostalInMemory.java
@@ -84,12 +84,12 @@ public class RepositorioEnderecoPostalInMemory implements Serializable, Reposito
         int totalEnderecosAdicionados = 0;
 
         for (EnderecoPostal enderecoPostal : outraListaEnderecoPostal.listaEnderecoPostal) {
-            boolean areaActividadeAdicionada = save(areaActividade);
-            if (areaActividadeAdicionada) {
-                totalAreasAdicionadas++;
+            boolean enderecoPostalAdicionado = save(enderecoPostal);
+            if (enderecoPostalAdicionado) {
+                totalEnderecosAdicionados++;
             }
         }
-        return totalAreasAdicionadas;
+        return totalEnderecosAdicionados;
 
     }
 }
