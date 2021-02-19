991e47d7c4a6e0ae665a3be912451cffcfed5f76
Ana Batista Pereira <anacbpereira91@upskill.pt>
2021-02-19 17:05:10 +0000

    update freelancer ui

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/FreelancerLogadoUI.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/FreelancerLogadoUI.java
@@ -1,13 +1,46 @@
 package com.grupo2.t4j.ui;
 
+import com.grupo2.t4j.model.Anuncio;
+import com.grupo2.t4j.model.Data;
+import com.grupo2.t4j.model.Tarefa;
+import com.grupo2.t4j.model.TipoRegimento;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class FreelancerLogadoUI implements Initializable {
+    
+     @FXML
+    private TableColumn<Anuncio, Data> inicioCandidatura;
+
+    @FXML
+    private TableColumn<Anuncio, Data> fimCandidatura;
+
+    @FXML
+    private TableColumn<Tarefa, String> tarefa;
+
+    @FXML
+    private TableColumn<Anuncio, Data> fimSeriacao;
+
+    @FXML
+    private TableView<Anuncio> tableCandidaturas;
+
+    @FXML
+    private TableColumn<Anuncio, Data> inicioPublicitacao;
+
+    @FXML
+    private TableColumn<Anuncio, Data> fimPublicitacao;
+
+    @FXML
+    private TableColumn<TipoRegimento, String> tipoRegimento;
+
+    @FXML
+    private TableColumn<Anuncio, Data> inicioSeriacao;
 
     private StartingPageUI startingPageUI;
     public void associarParentUI(StartingPageUI startingPageUI) {
