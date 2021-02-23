9947d6ff09e72278ca00f403ea658dfd78f465c6
Ana Batista Pereira <anacbpereira91@upskill.pt>
2021-02-23 00:25:44 +0000

    Consultar Area Actividade

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/ConsultarAreaActividadeUI.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/ConsultarAreaActividadeUI.java
@@ -6,9 +6,18 @@ package com.grupo2.t4j.ui;
  * and open the template in the editor.
  */
 
+import com.grupo2.t4j.controller.RegistarAreaActividadeController;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,13 +25,54 @@ import javafx.fxml.Initializable;
  * @author acris
  */
 public class ConsultarAreaActividadeUI implements Initializable {
+    
+    @FXML TextField txtDescBreve;
+    @FXML Button btnVoltar;
+    @FXML TextField txtCodigo;
+    @FXML TextArea txtDescDetalhada;
+    
+    
+    private AdministrativoLogadoUI administrativoLogadoUI;
+    private RegistarAreaActividadeController registarAreaActividadeController;
+    private Stage adicionarStage;
+    
+    public void associarParentUI(AdministrativoLogadoUI administrativoLogadoUI) {
+        this.administrativoLogadoUI = administrativoLogadoUI;
+    }
 
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        //try {
+
+            adicionarStage = new Stage();
+            adicionarStage.initModality(Modality.APPLICATION_MODAL);;
+            adicionarStage.setResizable(false);
+            
+            registarAreaActividadeController = new RegistarAreaActividadeController();
+            
+
+            
+        /*} catch (SQLException exception) {
+            exception.printStackTrace();
+        }*/
+
     }    
-    
+    public void transferData() throws SQLException {
+
+        txtCodigo.setText(administrativoLogadoUI.listaAreasActividade.getSelectionModel().getSelectedItem().getCodigo());
+        txtDescBreve.setText(administrativoLogadoUI.listaAreasActividade.getSelectionModel().getSelectedItem().getDescBreve());
+        txtDescDetalhada.setText(administrativoLogadoUI.listaAreasActividade.getSelectionModel().getSelectedItem().getDescDetalhada());
+        
+    }
+
+    @FXML
+    void voltarAtras(ActionEvent event) {
+        btnVoltar.getScene().getWindow().hide();
+    }
+
 }
