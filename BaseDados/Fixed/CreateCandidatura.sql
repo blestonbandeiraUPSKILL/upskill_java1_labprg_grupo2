42344babc7b4b64de3db10912f4d95a2f007b7a5
Bárbara Leston Bandeira <blestonbandeira@upskill.pt>
2021-02-25 22:31:43 +0000

    excel

--- BaseDados/Fixed/CreateCandidatura.sql
+++ BaseDados/Fixed/CreateCandidatura.sql
@@ -10,40 +10,14 @@ CREATE OR REPLACE PROCEDURE createCandidatura(
 
 IS
 
-    v_countAnuncio int;
-    v_countEmailFreelancer int;
-    ex_Anuncio exception;
-    ex_EmailFreelancer exception;
 
 BEGIN
 
-    SELECT count(*) INTO v_countAnuncio
-    FROM Anuncio
-    WHERE idanuncio = p_idAnuncio;
-    
-    IF v_countAnuncio = 0
-    THEN
-        RAISE ex_Anuncio;
-    END IF;
-    
-    SELECT count(*) INTO v_countEmailFreelancer
-    FROM Freelancer
-    WHERE email = p_emailFreelancer;
-    
-    IF v_countEmailFreelancer = 0
-    THEN
-        RAISE ex_EmailFreelancer;
-    END IF;
-    
     INSERT INTO Candidatura
         (dataFimCandidatura, valorPretendido, numeroDias, txtApresentacao, txtMotivacao, idAnuncio, emailFreelancer)
     VALUES
         (p_dataFimCandidatura, p_valorPretendido, p_numeroDias, p_txtApresentacao, p_txtMotivacao, p_idAnuncio, p_emailFreelancer);
     
-    EXCEPTION WHEN
-        ex_Anuncio OR ex_EmailFreelancer
-    THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Dados ienxistentes');
 
 END;
 /
