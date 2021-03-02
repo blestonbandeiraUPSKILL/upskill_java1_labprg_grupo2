11f308a22b7b6476baf563e838146efbba6f8db5
Bárbara Leston Bandeira <blestonbandeira@upskill.pt>
2021-03-02 09:06:57 +0000

    uc11 - revisão - done

--- BaseDados/it works.sql
+++ BaseDados/it works.sql
@@ -8,7 +8,11 @@ ON GrauProficiencia.idGrauProficiencia = CaracterCT.grauProfMinimo
 INNER JOIN Categoria 
 ON CaracterCT.codigoCategoria LIKE Categoria.codigoCategoria 
 INNER JOIN Tarefa 
-ON Categoria.codigoCategoria LIKE Tarefa.codigoCategoria;
+ON Categoria.codigoCategoria LIKE Tarefa.codigoCategoria
+INNER JOIN ReconhecimentoGP
+ON ReconhecimentoGP.idGrauProficiencia = GrauProficiencia.idGrauProficiencia
+INNER JOIN Freelancer
+ON ReconhecimentoGP.emailFreelancer LIKE Freelancer.email;
 
 select * from categoria;
 
@@ -26,3 +30,36 @@ on caracterct.codigocategoria LIKE categoria.codigocategoria
 inner join tarefa
 on categoria.codigocategoria LIKE tarefa.codigocategoria
 WHERE freelancer.email LIKE 'rosa@gmails.com'
+
+select * from categoria
+
+
+
+SELECT * FROM Tarefa 
+INNER JOIN Categoria 
+ON Categoria.codigoCategoria LIKE Tarefa.codigoCategoria 
+INNER JOIN CaracterCT 
+ON CaracterCT.codigoCategoria LIKE Categoria.codigoCategoria 
+INNER JOIN GrauProficiencia 
+ON CaracterCT.grauProfMinimo = GrauProficiencia.idGrauProficiencia 
+INNER JOIN ReconhecimentoGP 
+ON GrauProficiencia.idGrauProficiencia = ReconhecimentoGP.idGrauProficiencia 
+INNER JOIN Freelancer 
+ON ReconhecimentoGP.emailFreelancer LIKE Freelancer.email 
+WHERE freelancer.email LIKE 'rosa@gmails.com'
+AND Grauproficiencia.grau IN
+    ((SELECT GrauProficiencia.grau FROM GrauProficiencia
+    INNER JOIN ReconhecimentoGP 
+    ON ReconhecimentoGP.idGrauProficiencia = GrauProficiencia.idGrauProficiencia)
+    >= 
+    (SELECT GrauProficiencia.grau FROM GrauProficiencia
+    INNER JOIN CaracterCT
+    ON CaracterCT.grauProfMinimo = GrauProficiencia.idGrauProficiencia))
+AND GrauProficiencia.designacao IN
+    ((SELECT GrauProficiencia.designacao  FROM GrauProficiencia
+    INNER JOIN ReconhecimentoGP 
+    ON ReconhecimentoGP.idGrauProficiencia = GrauProficiencia.idGrauProficiencia)
+    = 
+    (SELECT GrauProficiencia.designacao FROM GrauProficiencia
+    INNER JOIN CaracterCT 
+    ON CaracterCT.grauProfMinimo = GrauProficiencia.idGrauProficiencia));
