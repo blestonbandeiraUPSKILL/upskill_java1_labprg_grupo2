5a4c32ee60709d3fc83bc636d458e0d4cd30b8b1
Marta Silvestre <marta.silvestre@upskill.pt>
2021-03-02 13:28:13 +0000

    Create EliminarCandidaturaUI.java

---
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/EliminarCandidaturaUI.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupo2.t4j.ui;
+
+import javafx.event.ActionEvent;
+
+/**
+ *
+ * @author marta
+ */
+public class EliminarCandidaturaUI {
+
+    public void eliminarCandidatura(ActionEvent actionEvent) {
+    }
+}
