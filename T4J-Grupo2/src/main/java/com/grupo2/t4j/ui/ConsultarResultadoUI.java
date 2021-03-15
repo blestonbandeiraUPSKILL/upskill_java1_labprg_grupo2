a56a4c6538676ebe66cc40905661a34c7baa130c
Caroline Duarte <duarte.carol@upskill.pt>
2021-03-15 15:27:45 +0000

    cena 13

---
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/ConsultarResultadoUI.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupo2.t4j.ui;
+
+/**
+ *
+ * @author CAD
+ */
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+public class ConsultarResultadoUI {
+    
+     @FXML
+    private TextField txtReferencia;
+
+    @FXML
+    private TextField txtCusto;
+
+    @FXML
+    private TextField txtDesignacao;
+
+    @FXML
+    private TextField txtIdAtribuicao;
+
+    @FXML
+    private TextField txtDtFimTarefa;
+
+    @FXML
+    private TextField txtDescTecnica;
+
+    @FXML
+    private TextField txtDtInTarefa;
+
+    @FXML
+    private TextField txtNomeOrganizacao;
+
+    @FXML
+    private TextField txtDataSeriacao;
+
+    @FXML
+    private TextField txtDescInformal;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private TextField txtEmailColaborador;
+
+    @FXML
+    private TextField txtIdAnuncio;
+
+    @FXML
+    private TextField txtClassificacao;
+
+    @FXML
+    private TextField txtEmailFreelancer;
+    
+     @FXML
+    void voltar(ActionEvent event) {
+
+    }
+}
