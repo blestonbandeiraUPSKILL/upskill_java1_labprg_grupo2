32f75eac6305d06b68ad44cd6871f08e3d26427b
Ana Batista Pereira <anacbpereira91@upskill.pt>
2021-03-16 09:55:53 +0000

    TarefaAtribuida

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/ConsultarResultadoUI.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/ConsultarResultadoUI.java
@@ -77,6 +77,6 @@ public class ConsultarResultadoUI {
      * @throws SQLException 
      */
     public void transferData() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //txtReferencia.setText(consultarResultadoController.findResultadoById());
     }
 }
