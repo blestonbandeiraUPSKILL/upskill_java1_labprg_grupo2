fefca105c286d66544ca47203b50753f1cf3a74f
Marta Silvestre <marta.silvestre@upskill.pt>
2021-03-16 11:05:51 +0000

    Update RepositorioAreaActividadeDatabase.java

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/persistence/database/RepositorioAreaActividadeDatabase.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/persistence/database/RepositorioAreaActividadeDatabase.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.grupo2.t4j.persistence.database;
 
 /**
@@ -19,10 +14,6 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Geral
- */
 public class RepositorioAreaActividadeDatabase implements RepositorioAreaActividade {
 
     /**
