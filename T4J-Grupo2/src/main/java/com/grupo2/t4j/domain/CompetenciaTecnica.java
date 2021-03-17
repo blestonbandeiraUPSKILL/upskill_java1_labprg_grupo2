d15356d362ddbfc08d359df1d1218778f181bd90
Bárbara Leston Bandeira <blestonbandeira@upskill.pt>
2021-03-17 12:42:25 +0000

    dtos

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/domain/CompetenciaTecnica.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/domain/CompetenciaTecnica.java
@@ -5,6 +5,8 @@
  */
 package com.grupo2.t4j.domain;
 
+import com.grupo2.t4j.dto.CompetenciaTecnicaDTO;
+import com.grupo2.t4j.dto.DTO;
 import com.grupo2.t4j.exception.AreaActividadeInexistenteException;
 import com.grupo2.t4j.exception.CodigoInvalidoException;
 import com.grupo2.t4j.exception.DescricaoInvalidaException;
@@ -16,7 +18,7 @@ import java.util.Objects;
  *
  * @author acris
  */
-public class CompetenciaTecnica implements Serializable {
+public class CompetenciaTecnica implements Serializable, DTO {
 
     /**
      * O codigo da competencia tencica
@@ -203,4 +205,8 @@ public class CompetenciaTecnica implements Serializable {
 
     }
 
+    @Override
+    public Object toDTO() {
+        return new CompetenciaTecnicaDTO(codigo, descricaoBreve, descricaoDetalhada, codigoAreaActividade);
+    }
 }
