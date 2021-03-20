c0968e5a7aea0b23400e842fdee1bd7a71f1e20c
Caroline Duarte <duarte.carol@upskill.pt>
2021-03-20 18:23:49 +0000

    strategy 1

--- T4J-Grupo2/src/main/java/com/grupo2/t4j/controller/AtribuirTarefaController.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/controller/AtribuirTarefaController.java
@@ -3,6 +3,8 @@ package com.grupo2.t4j.controller;
 import com.grupo2.t4j.domain.*;
 import com.grupo2.t4j.domain.strategy.*;
 import com.grupo2.t4j.dto.AtribuicaoDTO;
+import com.grupo2.t4j.dto.FreelancerDTO;
+import com.grupo2.t4j.dto.TarefaDTO;
 import com.grupo2.t4j.persistence.*;
 import com.grupo2.t4j.persistence.database.FabricaRepositoriosDatabase;
 
@@ -18,6 +20,9 @@ public class AtribuirTarefaController {
 
     private FabricaRepositorios fabricaRepositorios = new FabricaRepositoriosDatabase();
     private RepositorioAtribuicao repositorioAtribuicao = fabricaRepositorios.getRepositorioAtribuicao();
+    private RepositorioFreelancer repositorioFreelancer = fabricaRepositorios.getRepositorioFreelancer();
+    private RepositorioTarefa repositorioTarefa = fabricaRepositorios.getRepositorioTarefa();
+    private RepositorioSeriacao repositorioSeriacao = fabricaRepositorios.getRepositorioSeriacao();
 
     public AtribuirTarefaController(){
 
@@ -29,6 +34,26 @@ public class AtribuirTarefaController {
 
     public List<AtribuicaoDTO> getAllByOrganizacao(String nifOrganizacao) throws SQLException{
         return repositorioAtribuicao.getAllByOrganizacao(nifOrganizacao);
+    }
+
+    public ProcessoSeriacao getProcessoSeriacaoByAnuncio (int idAnuncio) throws SQLException{
+        return repositorioSeriacao.getProcessoSeriacaoByAnuncio(idAnuncio);
+    }
+
+    public TarefaDTO findTarefa(int idAnuncio) throws SQLException {
+        Tarefa tarefa = repositorioTarefa.findTarefa(idAnuncio);
+        return (TarefaDTO) tarefa.toDTO();
+    }
+
+    /**
+     * Devolve um freelancer a partir do seu email
+     * @param emailFree
+     * @return
+     * @throws SQLException
+     */
+    public FreelancerDTO findByEmail(String emailFree) throws SQLException{
+        Freelancer freelancer = repositorioFreelancer.findByEmail(emailFree);
+        return (FreelancerDTO) freelancer.toDTO();
     }
 
 }
--- T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/ColaboradorLogadoUI.java
+++ T4J-Grupo2/src/main/java/com/grupo2/t4j/ui/ColaboradorLogadoUI.java
@@ -2,12 +2,12 @@ package com.grupo2.t4j.ui;
 
 import com.grupo2.t4j.controller.*;
 import com.grupo2.t4j.domain.*;
-<<<<<<< HEAD
+
 import com.grupo2.t4j.dto.AtribuicaoDTO;
 import com.grupo2.t4j.dto.TarefaDTO;
-=======
+
 import com.grupo2.t4j.dto.*;
->>>>>>> 40ba634a463ef6f7775e912c931c501dd7bec644
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -78,12 +78,8 @@ public class ColaboradorLogadoUI implements Initializable {
     @FXML TableColumn<Object, Object> colunaDuracaoFree;
     @FXML TableColumn<Object, Object> colunaCustoFree;
 
-<<<<<<< HEAD
-    @FXML TableView<TabelaConsultaAtribuicao>  tabelaAtribuicoes;
-    List<TabelaConsultaAtribuicao> listaAtribuicoesOrganizacao = new ArrayList<>();
-=======
     @FXML TableView<TabelaConsultaAtribuicaoDTO>  tabelaAtribuicoes;
->>>>>>> 40ba634a463ef6f7775e912c931c501dd7bec644
+    List<TabelaConsultaAtribuicaoDTO> listaAtribuicoesOrganizacao = new ArrayList<>();
     @FXML TableColumn<Object, Object> colunaRefTarefa;
     @FXML TableColumn<Object, Object> colunaFreelancer;
     @FXML TableColumn<Object, Object> colunaCodAtribuicao;
@@ -383,7 +379,7 @@ public class ColaboradorLogadoUI implements Initializable {
         String nifOrganizacao = getNifOrganizacao();
         List<AtribuicaoDTO> atribuicoes = atribuirTarefaController.getAllByOrganizacao(nifOrganizacao);
         for(int i = 0; i < atribuicoes.size(); i++){
-            TabelaConsultaAtribuicao cellAtribuicao = new TabelaConsultaAtribuicao(atribuicoes.get(i).getRefTarefa(),
+            TabelaConsultaAtribuicaoDTO cellAtribuicao = new TabelaConsultaAtribuicaoDTO(atribuicoes.get(i).getRefTarefa(),
                     atribuicoes.get(i).getEmailFreelancer(), atribuicoes.get(i).getDataAtribuicao(),
                     atribuicoes.get(i).getCodigoAtribuicao());
             listaAtribuicoesOrganizacao.add(cellAtribuicao);
